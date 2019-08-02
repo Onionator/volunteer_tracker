b4446b0d63e92f3a4f26b7a8f9bb596ed09c4d5b
Sam Cook <WhenPotatoesFry@gmail.com>
2019-08-02 14:37:33 -0700

    pass first capybara test to create a project.

--- app.rb
+++ app.rb
@@ -6,8 +6,19 @@ require('./lib/project')
 require('./lib/volunteer')
 also_reload('lib/**/*.rb')
 
-DB = PG.connect({:dbname => "volunteer_tracker"})
+DB = PG.connect({:dbname => "volunteer_tracker_test"})
 
 get ('/') do
+  @projects = Project.all
   erb(:index)
+end
+
+post ('/new_project') do
+  Project.new({:title => params[:title], :id => nil}).save
+  redirect to('/')
+end
+
+get ('/project/:id') do
+  @project = Project.find(params[:id].to_i)
+  erb(:project)
 end
