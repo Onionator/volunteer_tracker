78f1385f211c64c7e5ba515c37b876ddff50ce09
Sam Cook <WhenPotatoesFry@gmail.com>
2019-08-02 13:03:04 -0700

    pass all volunteer_spec tests.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Volunteer
-  attr_accessor :name
+  attr_accessor :name, :project_id
   attr_reader :id
   def initialize(attritbutes)
     @name = attritbutes.fetch(:name)
@@ -9,7 +9,8 @@ class Volunteer
     @project_id = attritbutes.fetch(:project_id)
   end
   def save
-    DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id});")
+    volunteer = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = volunteer.first.fetch("id").to_i
   end
   def self.all
     volunteers = []
@@ -23,12 +24,19 @@ class Volunteer
     volunteers
   end
   def update(name, project_id)
-    @name = name.length > 0 ? name : @name
-    @project_id = project_id.length > 0 ? project_id : @project_id
+    @name = name
+    @project_id = project_id
     DB.exec("UPDATE volunteers SET name = '#{name}' WHERE id = #{@id};")
     DB.exec("UPDATE volunteers SET project_id = '#{project_id}' WHERE porject_id = #{@project_id};")
   end
   def delete
     DB.exec("DELETE FROM volunteers WHERE id = #{@id};")
+  end
+  def self.find(id)
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};")
+    Volunteer.new({:name => volunteer.first.fetch("name"), :id => id, :project_id => volunteer.first.fetch("project_id")})
+  end
+  def ==(compare)
+    self.name == compare.name
   end
 end
